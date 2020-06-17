c665976613bbc303a2184191c33b8caeec5c2fa1
Gary <gongxicheng1111@gmail.com>
2020-06-17 22:20:09 +0800

    Update server.js

--- dist/server.js
+++ dist/server.js
@@ -6,6 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const http_1 = __importDefault(require("http"));
 const config = require('../config.js');
-http_1.default.createServer(app_1.default).listen(8888, function () {
-    console.log('Example app listening on port 8888!');
+http_1.default.createServer(app_1.default).listen(8080, function () {
+    console.log('Example app listening on port 8080!');
 });
